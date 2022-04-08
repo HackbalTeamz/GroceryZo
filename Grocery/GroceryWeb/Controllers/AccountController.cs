cc945563c28c0f31a21225b8e0a0d7e7d28a1153
Abdulla Sirajudeen <abdullasirajudeen@gmail.com>
2022-04-08 23:08:28 +0530

    Web Project Updated

---
+++ Grocery/GroceryWeb/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string loginViewModel)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
